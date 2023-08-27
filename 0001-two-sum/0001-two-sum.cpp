70f2ad0de8b8fdcd7bebabb0b3eb3a1c6d3cce37
rehan-github <124896617+rehan-github@users.noreply.github.com>
2023-08-28 02:06:01 +0530

    Time: 8 ms (85.54%), Space: 11.1 MB (16.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans;
-        for(int i=0; i<nums.size();i++)
+        map<int, int> value;
+        for(int i=0; i<nums.size(); i++)
         {
-            int element = nums[i];
-            for(int j=i+1; j<nums.size();j++)
-            {
-                if(element + nums[j] == target)
-                {
-                    
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    // cout<<"["<<element<<"'"<<nums[j]<<"]";
-                    
-                    break;
-                }
-            }
+            int remaining = target - nums[i];
+            if(value.find(remaining) != value.end())
+                return{value[remaining],i};
+            value[nums[i]] = i;
         }
-        return ans;
+        return {};
     }
 };
